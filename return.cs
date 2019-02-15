15c1db35b314a8cdabbda4fe0a14f4fa1e83d417
Beka <bereketheramo25@gmail.com>
2019-02-16 01:45:50 +0300

    a java class which fetchs data form database is added

---
+++ return.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+namespace quizapp
+{
+    class @return
+    {
+        private string constring = "Data Source= localhost;Initial Catalog=quiz;Integrated Security=true;username=root;password=''";
+        public string scalarreturn(string q)
+        {
+            string s;
+            MySqlConnection conn = new MySqlConnection(constring);
+            conn.Open();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(q,conn);
+                s = cmd.ExecuteScalar().ToString();
+
+            }
+            catch (Exception)
+            {
+                
+                s = "";
+            }
+            return s;
+        }
+    }
+}
